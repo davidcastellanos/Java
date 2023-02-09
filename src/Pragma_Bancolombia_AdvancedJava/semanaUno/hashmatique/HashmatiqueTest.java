a5a2df69ee1fcc31e922676f08bb59f5c18b9e7d
davidcastellanos <castellanos.johann@gmail.com>
2023-02-09 17:14:45 -0500

    [UPDATE] Adding new challenge

--- src/Pragma_Bancolombia_AdvancedJava/semanaUno/hashmatique/HashmatiqueTest.java
+++ src/Pragma_Bancolombia_AdvancedJava/semanaUno/hashmatique/HashmatiqueTest.java
@@ -4,29 +4,13 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 public class HashmatiqueTest {
-
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Hashmatique hashmatique = new Hashmatique();
-        System.out.println("Agrega el número de canciones a incluir en el Track:");
-        int numOfTracks = scanner.nextInt();
-
-        HashMap<String, String> trackList = hashmatique.createHashMap();
-
-        int i = 1;
-        do {
-            System.out.println("Agrega el título de la canción:");
-            String keyMap = scanner.nextLine();
-            System.out.println("Agrega la letra de la canción:");
-            String value = scanner.nextLine();
-            trackList.put(keyMap, value);
-            i++;
-        } while (i <= numOfTracks);
-
-
-        System.out.println(trackList);
+        System.out.println(hashmatique.createTrackList());;
+        System.out.println("------------------------------");
+        System.out.println("La edad del cielo: \n" + hashmatique.findSongByTitle());
+        System.out.println("------------------------------");
+        hashmatique.printAllSongs();
 
     }
-
 }
