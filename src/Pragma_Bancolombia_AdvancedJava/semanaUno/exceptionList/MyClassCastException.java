da6081ca3be9c64da16b04718007fcad36397c68
davidcastellanos <castellanos.johann@gmail.com>
2023-02-09 17:20:33 -0500

    [UPDATE] Adding new exceptions challenge

---
+++ src/Pragma_Bancolombia_AdvancedJava/semanaUno/exceptionList/MyClassCastException.java
@@ -0,0 +1,24 @@
+package Pragma_Bancolombia_AdvancedJava.semanaUno.exceptionList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyClassCastException {
+    void testingArrayList(ArrayList<Object> arrayObjects) {
+        int castedValue = 0;
+
+        for(int i = 0; i < arrayObjects.size(); i++) {
+            try {
+                castedValue = (int) arrayObjects.get(i);
+
+            } catch (ClassCastException e) {
+                System.out.printf("Error en índice %d, no se puede castear %s\n", i, arrayObjects.get(i));
+                //throw new ClassCastException(String.format("Error en índice %d, no se puede castear %s\n", i, arrayObjects.get(i)));
+            }
+
+        }
+
+    }
+
+
+}
---
+++ src/Pragma_Bancolombia_AdvancedJava/semanaUno/exceptionList/MyClassCastExceptionTest.java
@@ -0,0 +1,16 @@
+package Pragma_Bancolombia_AdvancedJava.semanaUno.exceptionList;
+
+import java.util.ArrayList;
+
+public class MyClassCastExceptionTest {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("Hola Mundo");
+        myList.add(48);
+        myList.add("Adios Mundo");
+
+        MyClassCastException myClassCastException = new MyClassCastException();
+        myClassCastException.testingArrayList(myList);
+    }
+}
